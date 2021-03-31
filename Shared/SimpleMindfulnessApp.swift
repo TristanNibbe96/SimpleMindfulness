16da837a414f756445a7be821194167a1a5737b1
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-03-31 00:05:55 -0700

    Initial Commit

---
+++ Shared/SimpleMindfulnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMindfulnessApp.swift
+//  Shared
+//
+//  Created by user192295 on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMindfulnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

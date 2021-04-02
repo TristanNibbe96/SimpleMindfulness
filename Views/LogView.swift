bc1e391cdf5da60e6ba80f42899fcf7fcd84e590
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-04-01 17:04:50 -0700

    Added basic tab bar with navigation

---
+++ Views/LogView.swift
@@ -0,0 +1,20 @@
+//
+//  LogView.swift
+//  SimpleMindfulness
+//
+//  Created by user192295 on 4/1/21.
+//
+
+import SwiftUI
+
+struct LogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogView()
+    }
+}
---
+++ Views/TabBarView.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarView.swift
+//  SimpleMindfulness
+//
+//  Created by user192295 on 4/1/21.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        TabView{
+            ContentView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "book.circle")
+                    Text("Log")
+                }
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}

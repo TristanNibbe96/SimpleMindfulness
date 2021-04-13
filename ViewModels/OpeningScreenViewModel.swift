fc79f300f7ac6572d0b7b6d59bd922c64fe508e7
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-04-12 23:35:47 -0700

    Started working on adding privacy policy as txt file and reading it in

---
+++ ViewModels/OpeningScreenViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  OpeningScreenViewModel.swift
+//  SimpleMindfulness
+//
+//  Created by Tristan Nibbe on 4/12/21.
+//
+
+import Foundation
+
+class OpeningScreenViewModel: ObservableObject{
+    
+}

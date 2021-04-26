ea65908a059b0a5e96a4b7f9a45b0fc5459e5bac
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-04-26 10:20:28 +0200

    started adding backend to save and load logged emotions

---
+++ ViewModels/New Group/CoreDataManager.swift
@@ -0,0 +1,34 @@
+//
+//  CoreDataManager.swift
+//  SimpleMindfulness
+//
+//  Created by Tristan Nibbe on 4/26/21.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager{
+    static let shared = CoreDataManager()
+    
+    private init(){
+        
+    }
+    
+    private lazy var persistantContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "LoggedEmotions")
+        container.loadPersistentStores(completionHandler: { _, error in
+            _ = error.map {fatalError("Unresolved error \($0)")}
+        })
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext{
+        return persistantContainer.viewContext
+    }
+    
+    func backgroundContext() -> NSManagedObjectContext{
+        return persistantContainer.newBackgroundContext()
+    }
+    
+}
